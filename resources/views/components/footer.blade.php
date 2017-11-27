73bb938ad7f4fb3900fbfc74adf17eb846a38030
Jessica Mundt <j.mundt@arcane.ws>
2017-11-27 14:05:14 -0500

    Global views (non client specific) file strcuture updated

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,67 @@
+<footer id="footer">
+    <div class="footerTop">
+        <div class="footerNavigationContainer">
+            <nav>
+                <p class="footerNavTitle">MaxLiving</p>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Locations</a></li>
+                    <li><a href="#">Doctors / Students</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Press & Media</a></li>
+                    <li><a href="#">Security & Privacy</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <p class="footerNavTitle">Store</p>
+                <ul>
+                    <li><a href="#">Shipping & Returns</a></li>
+                    <li><a href="#">Guarantee</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Order Subscription</a></li>
+                    <li><a href="#">My Account</a></li>
+                    <li><a href="#">Become an Affiliate</a></li>
+                    <li><a href="#">Become a Wholesaler</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="footerContent">
+            <div class="footerSocial">
+                <ul>
+                    <li>
+                        <a href="#" class="socialIcon icon-facebook" title="Facebook">
+                            <span class="invisible">Facebook</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="socialIcon icon-twitter" title="Twitter">
+                            <span class="invisible">Twitter</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="socialIcon icon-googleplus" title="Google+">
+                            <span class="invisible">Google+</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footerCredit">
+                <div class="footerCreditContent">
+                    <p>&copy; Copyright 2017 MaxLiving. All rights reserved.</p>
+                    <p>The 5 Essentials is a registered trademark of MaxLiving.</p>
+                </div>
+                <div class="footerCreditContent">
+                    <p><strong>Disclaimer:</strong> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil .</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footerBottom">
+        <nav>
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" data-client="error">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name', 'Maximized Living Store') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!--
+    Currently fonts are loaded in with app.css - we need to look into how to load this separately.
+    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+    -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="app">
-    @yield('content')
-</div>
+    @include('client_store/components/header')
 
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+    <main id="app">
+        @yield('content')
+    </main>
+
+    @include('client_store/components/footer')
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
