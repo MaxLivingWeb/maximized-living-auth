45a70f5d1df607eb8d91816b401c3b91a5edfc61
Jessica Mundt <j.mundt@arcane.ws>
2017-11-27 17:08:15 -0500

    Login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
-        <div class="col-md-6 col-md-push-3">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Login</h3>
-                </div>
-                <div class="panel-body">
-                    @if($errors->any())
-                        @include('components/forms/form-alert', ['errors' => $errors->all()])
-                    @endif
+    <section class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-push-3">
+                <div class="loginPanel panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Login</h3>
+                    </div>
+                    <div class="panel-body">
+                        @if($errors->any())
+                            @include('components/forms/form-alert', ['errors' => $errors->all()])
+                        @endif
 
-                    @include('components/forms/login-form')
+                        @include('components/forms/login-form')
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
