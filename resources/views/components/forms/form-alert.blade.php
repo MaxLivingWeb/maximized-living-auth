98b1c381dda8e3836b0b662f6dc808a9c134cbf1
Jessica Mundt <j.mundt@arcane.ws>
2017-11-28 12:39:23 -0500

    Styled login view to actually match styleguide styles

--- resources/views/components/forms/form-alert.blade.php
+++ resources/views/components/forms/form-alert.blade.php
@@ -1,5 +1,9 @@
-@foreach($errors as $error)
-    <div class="alert alert-danger">
-        <p>{{ $error }}</p>
+@if (count($errors) > 0)
+    <div class="formErrors">
+        @foreach($errors as $error)
+            <div class="alert alert-danger">
+                <p>{{ $error }}</p>
+            </div>
+        @endforeach
     </div>
-@endforeach
+@endif
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-    <section class="page-header text-center">
+    <section class="heroAlternative heroAlternative-padded centerAlign border-faintGrey">
         <div class="container">
-            <h1>My Account</h1>
+            <div class="heroContent">
+                <h1 class="heroHeadline">My Account</h1>
+            </div>
         </div>
     </section>
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-push-3">
-                    <div class="panel panel-primary panel-shadow panel-pushdown">
-                        <div class="panel-heading">
-                            <h2 class="panel-title">Login</h2>
-                        </div>
-                        <div class="panel-body">
-                            @if($errors->any())
-                                @include('components/forms/form-alert', ['errors' => $errors->all()])
-                            @endif
+    <section class="userFormSection">
+        <div class="bubbleCardContainer">
+            <div class="left bubbleBgContainer">
+                <img src="../images/BubbleBg-Left.png"/>
+            </div>
+            <div class="container">
+                <div class="userFormCard card">
+                    <h2>Login</h2>
+                    <p>If you have an account, please login below</p>
 
-                            @include('components/forms/login-form')
-                        </div>
-                    </div>
+                    @if($errors->any())
+                        @include('components/forms/form-alert', ['errors' => $errors->all()])
+                    @endif
+
+                    @include('components/forms/login-form')
                 </div>
+            </div>
+            <div class="right bubbleBgContainer">
+                <img src="../images/BubbleBg-Right.png"/>
             </div>
         </div>
     </section>
