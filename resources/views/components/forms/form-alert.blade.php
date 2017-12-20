07806d4ff82420afea05864dfd615da182965116
Jessica Mundt <j.mundt@arcane.ws>
2017-12-20 10:58:36 -0500

    Tidied up form-alert component

--- resources/views/components/forms/form-alert.blade.php
+++ resources/views/components/forms/form-alert.blade.php
@@ -1,19 +1,15 @@
 @if (count($errors) > 0)
-    <div class="formErrors">
-        @foreach($errors as $error)
-            <div class="alert alert-danger">
-                <p>{!! $error !!}</p>
-            </div>
-        @endforeach
-    </div>
+    @foreach($errors as $error)
+        <div class="alert alert-danger">
+            <p>{!! $error !!}</p>
+        </div>
+    @endforeach
 @endif
 
 @if (isset($messages))
-    <div class="formErrors">
-        @foreach($messages as $message)
-            <div class="alert alert-success">
-                <p>{!! $message !!}</p>
-            </div>
-        @endforeach
-    </div>
+    @foreach($messages as $message)
+        <div class="alert alert-success">
+            <p>{!! $message !!}</p>
+        </div>
+    @endforeach
 @endif
