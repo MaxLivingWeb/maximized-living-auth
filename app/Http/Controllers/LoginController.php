4b2ab31df77f46dd3f8f834bfc8e9f7819583d44
Jessica Mundt <j.mundt@arcane.ws>
2018-04-04 11:14:15 -0400

    Copy updates (provided from Paul)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
                 return redirect()->back()
                     ->withInput()
                     ->withErrors([
-                        'We regret your account has been deactivated and your login doesn\'t appear to be working anymore.<br><br>For more information, please contact MaxLiving\'s Web Support for assistance by calling <a href="tel:3219392040">321-939-2040</a> or&nbsp;emailing&nbsp;<a href="mailto:websupport@maxliving.com">websupport@maxliving.com</a>'
+                        'Your account has been suspended.<br><br>You can log in by creating a new account. Please reach out to MaxLiving support if you have questions.<br><br>Tel: <a href="tel:3219392040">(321) 939-2040</a><br>Email: <a href="mailto:websupport@maxliving.com">websupport@maxliving.com</a>'
                     ]);
             }
 
